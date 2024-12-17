d2cd40d469eea52ac1e6afcdf850cd8b0d06dfb7
Ryan James <ryanjames1729@gmail.com>
2024-12-17 10:59:22 -0500

    initial commit

---
+++ Main.java
@@ -0,0 +1,72 @@
+// Array Cretion and Access      A+CR
+class Main {
+    public static void main(String[] args) {
+      
+          // EXAMPLE 1
+          int[] intArray = new int[10];
+          for(int i = 0; i < intArray.length; i++){
+              System.out.print(intArray[i] + " ");
+      }
+        System.out.println();
+  
+     
+      
+          // Below create your own array of int data types and print it.
+      
+  
+  
+  
+          //EXAMPLE 2
+           //int[] intArray2 = new int[10];
+           //System.out.println(intArray2[10]);		
+          // Go into the example above and fix it so that it doesn't print the ArrayIndexOutOfBoundsException. 
+  
+  
+  
+          //EXAMPLE 3
+          // int[] intArray3 = {1,3,5,7,9};
+          // for(int i = 0; i < intArray3.length; i++){
+          // 	System.out.print(intArray3[i] + " ");
+          // }
+          // System.out.println();
+          //Below create your own array of int data types and print it.
+  
+  
+  
+  
+          // EXAMPLE 4
+          // double[] doubleArray = new double[5];
+          // doubleArray[2] = 3.14;
+          // for(int i = 0; i < doubleArray.length; i++){
+          // 	System.out.print(doubleArray[i] + " ");
+          // }
+          // System.out.println();
+          // Below create your own array of double data types and print it.
+  
+  
+  
+          // EXAMPLE 5
+          // Create an array of boolean values and print it the screen below.
+  
+  
+  
+          // EXAMPLE 6
+          // String[] stringArray = new String[5];
+          // stringArray[0] = "AP";
+          // stringArray[1] = "Computer";
+          // stringArray[2] = "Science";
+          // stringArray[3] = "A";
+          // for(int i = 0; i < stringArray.length; i++){
+          // 	System.out.print(stringArray[i] + " ");
+          // }
+          // System.out.println();
+  
+          // Go into the example above and fix it so that it doesn't print 'null'. 
+  
+          
+  
+          
+    
+      
+      }
+  }
